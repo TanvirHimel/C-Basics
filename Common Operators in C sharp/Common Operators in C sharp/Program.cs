857194215e9bf6e41addcefe139dff1db4ecbcb7
TanvirHimel <tanvirahimel@gmail.com>
2020-09-23 11:04:33 +0600

    Second commit

---
+++ Common Operators in C sharp/Common Operators in C sharp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common_Operators_in_C_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numerator = 10;
+            int denominator = 5;
+
+            int division = numerator / denominator;
+            int modulas = numerator % denominator;
+
+            Console.WriteLine(" Division of {0} and {1} is {2} and modulas is {3}", numerator, denominator, division, modulas);
+
+            if(numerator == 5 || denominator == 10 && numerator<denominator)
+            {
+                Console.WriteLine(" {0}/{1} is a proper fraction", numerator, denominator);
+            }
+            else
+                Console.WriteLine(" {0}/{1} is an improper fraction", numerator, denominator);
+            bool IsNumber10 = numerator == 10 ? true : false;
+            Console.WriteLine(" IsNumber10 is {0}", IsNumber10);
+        }
+    }
+}
--- Part 4 String type in C sharp/Part 4 String type in C sharp/Program.cs
+++ Part 4 String type in C sharp/Part 4 String type in C sharp/Program.cs
@@ -10,6 +10,14 @@ namespace Part_4_String_type_in_C_sharp
     {
         static void Main(string[] args)
         {
+            string name = "\"Kudvengkat\"\n";
+            string name2 = "One\nTwo\nThree\n";
+            //if there is @ before any string then visual studio ignores all the escape sequences within the strings
+            //which is known as Varbatim Literal
+            string name3 = @"C:\Users\USER\source\repos\Kudvenkat";
+            Console.Write(name);
+            Console.Write(name2);
+            Console.WriteLine(name3);
         }
     }
 }
