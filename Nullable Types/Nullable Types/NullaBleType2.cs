25d97179031453f0062e511a8a80d447ef70b46e
TanvirHimel <tanvirahimel@gmail.com>
2020-09-24 11:32:35 +0600

    third commit

---
+++ Nullable Types/Nullable Types/NullaBleType2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nullable_Types
+{
+    public class NullaBleType2
+    {
+        public void Nullable()
+        {
+            int AvailableTickets;
+            int? TicketsOnSale = null;
+
+            //without null coalescing operator
+            /*
+            if (TicketsOnSale == null)
+                AvailableTickets = 0;
+            else
+                AvailableTickets = TicketsOnSale.Value;
+
+            Console.WriteLine("Availabe tickets = {0}", AvailableTickets);
+            */
+
+            //with null coalescing operator
+            AvailableTickets = TicketsOnSale ?? 0;
+            Console.WriteLine("Availabe tickets = {0}", AvailableTickets);
+        }
+    }
+}
--- Nullable Types/Nullable Types/Program.cs
+++ Nullable Types/Nullable Types/Program.cs
@@ -6,11 +6,31 @@ using System.Threading.Tasks;
 
 namespace Nullable_Types
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            string name = null;
+            int i = 0;
+            //to store null values in a varibale put question mark after data type
+            int? j = null;
 
+            bool? IsMajor = null;
+            
+            if (IsMajor == true)
+            {
+                Console.WriteLine("User is major.");
+            }
+            else if (IsMajor == false)
+            {
+                Console.WriteLine("User is not major.");
+            }
+            else
+                Console.WriteLine("User did not answer the question.");
+
+            NullaBleType2 nul = new NullaBleType2();
+
+            nul.Nullable();
         }
     }
 }
